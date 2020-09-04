bb0e82a94a20e42fc42311f6430f214ce5466783
Muhammad Ali Umer <muhammad.ali.umber@directlinegroup.co.uk>
2020-09-04 17:21:57 +0100

    add new files.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 var bodyParser = require("body-parser");
 var express = require('express');
 var cors = require('cors');
 var questionnaire = require('./app/questionnaire/router');
 var students = require('./app/students/router');
 var subjects = require('./app/subjects/router');
+var jwt = require('jsonwebtoken');
 var app = express();
 
 app.use(bodyParser.json());
@@ -11,10 +13,28 @@ app.use(cors());
 
 // See the README about ordering of middleware
 // Load the routes ("controllers" -ish)
-app.use('/questionnaire', questionnaire);
+app.use('/questionnaire', authenticateToken ,questionnaire);
 app.use('/students', students);
 app.use('/subjects', subjects);
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({accessToken: accessToken});
+});
 
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token === null ) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+
+}
 // FINALLY, use any error handlers
 // app.use(require('app/errors/not-found'))
 
